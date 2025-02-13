819b90750d710782d5eb5ccfef96d972e62ee40b
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-02-14 00:16:57 +0300

    Part 4 done Added gaming logic: submit, clear, press letter methods

--- SwiftyWords(Project8)/ViewController.swift
+++ SwiftyWords(Project8)/ViewController.swift
@@ -29,14 +29,55 @@ class ViewController: UIViewController {
     }
     
     @objc func letterTapped(_ sender: UIButton) {
-        
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
     
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
         
+        for buttion in activatedButtons {
+            buttion.isHidden = false
+        }
+        activatedButtons.removeAll()
     }
     
     @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
+        
+        if let solutionPosition = solutions.firstIndex(of: answerText) {
+            activatedButtons.removeAll()
+            
+            var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+            splitAnswers?[solutionPosition] = answerText
+            answersLabel.text = splitAnswers?.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Yes", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+        
+        loadLevel()
+        
+        for button in letterButtons {
+            button.isHidden = false
+        }
         
     }
     
